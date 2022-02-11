aa6b8f44b37f71053f0dd5ddef413d17b7fae3f0
safabalo <safaabalha58@gmail.com>
2022-02-11 10:59:45 +0000

    first

---
+++ projet/payment.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="style/style.css">
+    <script src="js/bootstrap.bundle.min.js" defer></script>
+    <title>Payment</title>
+</head>
+<body class="body">
+<?php
+    include 'navbar.php'
+?>
+    <?php
+        include 'sidebar.php'
+    ?>
+    <main class="main float-end mt-4">
+        <article class="article_table">
+            <div class=" mb-3 mt-5 d-flex justify-content-between student__list ms-2">
+                <h2 class="h1">Payment details</h2>
+                <div>
+                    <a href="addpayment.php" class="btn btn-info text-white px-4 py-2 me-1">Add payment</a>
+                    <img src="img/Up-Down.svg" alt="up_down">
+                </div>
+                
+            </div>
+            <table class="table table-striped ms-4 bg-white payment__table">
+                <thead>
+                    <tr class="table-head">
+                        <th scope="col">Name</th>
+                        <th scope="col">Payment scheduel</th>
+                        <th scope="col">Bill number</th>
+                        <th scope="col">Amount Paid</th>
+                        <th scope="col">Balance amount</th>
+                        <th scope="col">Date</th>
+                        <th scope="col"></th>
+                      </tr>
+                </thead>
+                <tbody>
+
+
+                <?php 
+                      $mysqli = new mysqli('localhost', 'root', '', 'studentdb') or die (mysqli_error($mysqli));
+                        $payments= $mysqli->query ("SELECT * FROM  mypayments") or die($mysqli->error);
+                    foreach($payments as $payment) : ?>
+                  <tr>
+                    <th scope="row"><?php echo $payment["name"]?></th>
+                    <td><?php echo $payment["payment_scheduel"]?></td>
+                    <td><?php echo $payment["bill_number"]?></td>
+                    <td>DHS <?php echo $payment["amount_paid"]?></td>
+                    <td>DHS <?php echo $payment["balance_amount"]?></td>
+                    <td><?php echo $payment["date_paying"]?></td>
+                    <td class="text-info">
+                    <a href="addpayment.php?edit=<?php echo $payment['id']; ?>">
+                        <img src="img/Eye.svg" alt="">
+                        </a>
+                    </td>
+                   
+                  
+                    
+                  <?php endforeach ?>
+                 
+                  
+                </tbody>
+              </table>
+            </article>
+    </main>
+</body>
+</html>
---
+++ projet/student.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="style/style.css">
+    <script src="js/bootstrap.bundle.min.js" defer></script>
+    <title>Student</title>
+</head>
+<body class="body">
+<?php
+    include 'navbar.php';
+?>
+    <?php
+        include 'sidebar.php';
+    ?>
+<main class="main float-end mt-4">
+        <article class="" >
+            <div class="d-flex justify-content-between student__list ms-2">
+                <h2 class="h1">Student list</h2>
+                <div>
+                    <img src="img/Up-Down.svg" alt="" class="me-2">
+                    <a class="btn btn-info text-white" href="addstudent.php">ADD NEW STUDENT</a>
+                </div>
+                
+            </div>
+            
+
+            <table class="table student_table ">
+                <thead class="table-head">
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col" class="thead__elements">Name</th>
+                        <th scope="col" class="thead__elements">Email</th>
+                        <th scope="col" class="thead__elements">Phone</th>
+                        <th scope="col" class="thead__elements">Enroll Number</th>
+                        <th scope="col" class="thead__elements">Date of admission</th>
+                    </tr>
+                </thead>
+                  <tbody>
+                        
+                              
+                                <?php 
+                                $mysqli = new mysqli('localhost', 'root', '', 'studentdb') or die (mysqli_error($mysqli));
+                                $users= $mysqli->query ("SELECT * FROM  mystudents") or die($mysqli->error);
+                                foreach($users as $user) : 
+                                   ?>
+                                    <tr class="table__elements">
+                                    <th scope="row"><img src="img/user-profile.png" alt="user_svg"></th>
+                                    <td><?php echo $user['name'];?></td>
+                                    <td><?php echo $user['email'];?></td>
+                                    <td><?php echo $user['phone'];?></td>
+                                    <td><?php echo $user['enroll_number'];?></td>
+                                    <td><?php echo $user['date_of_admission'];?></td>
+                                    <td>
+                                        <a href="addstudent.php?edit=<?php echo $user['id']; ?>">
+                                        <img src="img/Pen.svg" alt="edit">
+                                        </a>
+                                        
+                                    </td>
+                                    <td>
+                                        <a href="crudstudent.php?delete=<?php echo $user['id']; ?>">
+                                        <img src="img/Garbage.svg" alt="delete">
+                                        </a>
+                                        
+                                    </td>
+                                    </tr>
+                                <?php endforeach ?>
+                  </tbody>
+                </table>
+            </article>    
+</body>
+</html>
